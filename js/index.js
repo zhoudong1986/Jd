66741f66682e3f489a6010d8ca2b122d53c4672d
zhoudong1986 <11273548@qq.com>
2015-07-16 19:34:38 +0800

    2015/7/16上传

--- js/index.js
+++ js/index.js
@@ -2,9 +2,20 @@
  * Created by Administrator on 2015/7/7.
  */
 
-var index = null;
+var $index = null;
+var $sum = 0 ;
+var $timer = null;
+function doTurn($plg){
+  var $leng = $('ul.indexBox').children('li').size();
+  if($sum>=$leng){$sum=0};
+  if($sum<0){$sum=$leng-1};
+  $('ul.indexBox').children('li').eq($sum).css({backgroundColor:'red'}).siblings('li').css({backgroundColor:'#3e3e3e'});
+  $('.imgsBox').children('a.imgsA').eq($sum).show().siblings('a.imgsA').hide();
+  if(!$plg){$sum++;}
+}
+
 $(function(){
-  //顶部广告点击消失事件
+
   $('#banner').children('i').hover(function(){
     $(this).css({backgroundPosition:'0 -19px'});
   },function(){
@@ -13,14 +24,14 @@ $(function(){
     $(this).parent('#banner').fadeOut(300);
   });
 
-  //购物车
+
   $('.content').hover(function(){
     $(this).css({backgroundColor:'#fff'}).children('.line,.under').show();
   },function(){
     $(this).css({backgroundColor:'#f9f9f9'}).children('.line,.under').hide();
   });
 
-  //二级菜单事件
+
   $('.class').children('div').children('div').hover(function(){
     $index = $(this).index();
     $('.dd-inner .item').eq($index).css({left:'1px',backgroundColor:'#fff'}).find('h3 a').css('color','red');
@@ -29,6 +40,40 @@ $(function(){
   },function(){
     $('.dd-inner .item').eq($index).css({left:'0px',backgroundColor:''}).find('h3 a').css('color','#fff');
     $('.dorpdown-layer').hide();
-    $('div.item-sub').eq($index).siblings().hide();
+    $('div.item-sub').eq($index).hide();
   });
+
+
+  $timer = setInterval('doTurn()',2000);
+  $('.turnBox').hover(function(){
+    clearTimeout($timer);
+    $timer =null;
+    $('.peak').show();
+  },function(){
+    $timer = setInterval('doTurn()',2000);
+    $('.peak').hide();
+  });
+  $('div.peak').children('div').click(function(){
+    var $dir = $(this).attr('class');
+    var $speed = 1;
+    if($dir=='left'){$speed*=-1;}
+    $sum = parseInt($sum)+parseInt($speed);
+    doTurn(true);
+  });
+
+  $('ul.indexBox li').mouseover(function(){
+    clearTimeout($timer);
+    $timer =null;
+     var $index =  $(this).index();
+     $sum = $index;
+    doTurn(true);
+  });
+
+  $('#select-price-money').change(function(){
+    var $index = $('#select-price-money option:selected').index();
+    $('.msg-price').eq($index).addClass('hover').siblings('span').removeClass('hover');
+  })
+
+
+
 });
