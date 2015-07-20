d24ac037f23b5dbe73e4700f7db91daa7edcae44
zhoudong1986 <11273548@qq.com>
2015-07-20 08:27:26 +0800

    2015/7/20-yoyo

--- shop_jd/Home/Conf/config.php
+++ shop_jd/Home/Conf/config.php
@@ -1,4 +1,10 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'TMPL_PARSE_STRING' => array(
+        '__HOMEPUBLIC__' => __ROOT__.'/'.APP_NAME.'/Home/Public',
+        '__CSS__' => __ROOT__.'/'.APP_NAME.'/Home/Public/css',
+        '__JS__' => __ROOT__.'/'.APP_NAME.'/Home/Public/js',
+        '__IMAGE__' => __ROOT__.'/'.APP_NAME.'/Home/Public/images',
+    ),
 );
